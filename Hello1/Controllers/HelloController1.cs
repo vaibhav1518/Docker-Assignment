5f40c047d813bf53677c98cd716543812c2a9ec4
vaibhav1518 <vaibhav.soni@nagarro.com>
2024-09-16 14:24:45 +0530

    Docker Assignment

---
+++ Hello1/Controllers/HelloController1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hello1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController1 : ControllerBase
+    {
+        // GET: api/helloworld
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World from controller 1");
+        }
+    }
+}
